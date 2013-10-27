be44956d256c3c019103acf2740b62e30f3c82d5
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2013-10-28 01:15:55 +0400

    intro translated

---
+++ english/c.bat
@@ -0,0 +1,9 @@
+latex mix.tex
+latex mix.tex
+dvipdfm mix.dvi
+java -Xmx128M -Xss128M Gen
+latex mixc.tex
+latex mixc.tex
+dvipdfm mixc.dvi
+copy mixc.pdf D:\Dropbox\PhD\english
+copy mixc.tex D:\Dropbox\PhD\english
