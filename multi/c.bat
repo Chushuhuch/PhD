10523a602dd116a05a772e096c51e5d26ad8e184
Sergey.Bankevich <Sergey.Bankevich@jetbrains.com>
2014-03-24 01:33:51 +0400

    initial multidimentional case

---
+++ multi/c.bat
@@ -0,0 +1,9 @@
+latex mix.tex
+latex mix.tex
+dvipdfm mix.dvi
+java -Xmx128M -Xss128M Gen
+latex mixc.tex
+latex mixc.tex
+dvipdfm mixc.dvi
+copy mixc.pdf D:\Dropbox\PhD
+copy mixc.tex D:\Dropbox\PhD
