e69dedb9f3b62f06f9b783ba5fde64f62127c8a7
Sergey.Bankevich <Sergey.Bankevich@jetbrains.com>
2014-10-27 11:36:34 +0300

    corrections

--- multi/c.bat
+++ multi/c.bat
@@ -5,5 +5,5 @@ java -Xmx128M -Xss128M Gen
 latex mixc.tex
 latex mixc.tex
 dvipdfm mixc.dvi
-copy mixc.pdf D:\Dropbox\PhD
-copy mixc.tex D:\Dropbox\PhD
+copy mixc.pdf C:\downloads\Dropbox\PhD\multi
+copy mixc.tex C:\downloads\Dropbox\PhD\multi
