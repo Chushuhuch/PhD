263f870e313cd45852760821aee9d37c6b4e87d4
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2017-01-15 04:34:05 +0300

    machine translation

---
+++ multi/c.sh
@@ -0,0 +1,5 @@
+pdflatex mix.tex
+pdflatex mix.tex
+java -Xmx128M -Xss128M Gen
+pdflatex mixc.tex
+pdflatex mixc.tex
---
+++ multi/english/c.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+pdflatex mix.tex
+pdflatex mix.tex
+#java -Xmx128M -Xss128M Gen
+#pdflatex mixc.tex
+#pdflatex mixc.tex
